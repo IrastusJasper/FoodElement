4b4ad886e8bbef479e26dc3756a85c151af60bb4
IrastusJasper <jasperirastus@gmail.com>
2020-07-29 21:15:48 +0530

    master

--- project/jQuery.js
+++ project/jQuery.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
-    $('h1').click(function(){
-        $(this).css('background-color','red');
-    })
+   
+   $('.js--section-feature').waypoint(function(direction){
+       if(direction=='down'){
+           $('nav').addClass('sticky');
+       }else{
+           $('nav').removeClass('sticky')
+       }
+   },{
+       offset: '60px;'
+   });
+    $('.js--scroll-to-plan').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top},1000);
+    });
+
+    $('.js--scroll-to-start').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-feature').offset().top},1000);
+    });
 });
