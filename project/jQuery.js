982a96aee01c15721a5b7bf82d6efe807eae9acf
IrastusJasper <jasperirastus@gmail.com>
2020-08-03 21:25:27 +0530

    master

--- project/jQuery.js
+++ project/jQuery.js
@@ -39,4 +39,20 @@ $(document).ready(function(){
         offset: '50%'
     });
 
+    /*mobile nav*/
+    $('.js--nav-icon').click(function(){
+        var nav =$('.js--main-nav');
+        var nav =$('.js--nav-icon i');
+
+        nav.slideToggle(200);
+
+        if (icon.hasClass('ion-navicon-round')){
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-class-round');
+        }
+    });
+
 });
